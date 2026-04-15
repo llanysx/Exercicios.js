b7a4c9e8e0d10272b16a1150dfc04068576bb764
Lany <allanyaparecidapereira@gmail.com>
2026-04-15 10:18:23 -0300

    Update Exercicio1.js

---
+++ Exercicio1.js
@@ -0,0 +1,9 @@
+let filme1 = 120
+let filme2 = 95
+let filme3 = 110
+
+let tempoTotal = (filme1+ filme2+ filme3)
+let media = (tempoTotal/3)
+
+console.log ("O tempo total é:", tempoTotal );
+console.log ("A média de duração dos filmes é:", media);
