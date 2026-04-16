b68c4e9ddc717f96ef1bfe0619abe43709242abd
Lany <allanyaparecidapereira@gmail.com>
2026-04-15 22:52:30 -0300

    Atualizar o Exercicio2.js

---
+++ Exercicio2.js
@@ -0,0 +1,11 @@
+let vida= 100;
+let dano= 30;
+
+let vidaRestante = vida - dano;
+
+if (vidaRestante > 0) {
+    console.log ("Você ainda está vivo!\nVida:",vidaRestante)
+} else {
+    console.log("Game over")
+}
+
