8f91de138aa5790c17370964eecdf74706950e97
Lany <allanyaparecidapereira@gmail.com>
2026-04-15 23:05:47 -0300

    Atualizar o Exercício5.js

--- Exercício5.js
+++ Exercício5.js
@@ -1,1 +1,12 @@
-.
+let gol1 = 2;
+let gol2 = 1;
+let gol3 = 3;
+let gol4 = 0;
+let gol5 = 4;
+
+let total = gol1 + gol2 + gol3 + gol4 + gol5;
+
+let media = total / 5;
+
+console.log("O total de gols marcados pelo time é:", total);
+console.log("E a média de gols por partida é:", media);
